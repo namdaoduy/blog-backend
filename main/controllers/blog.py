1621f6fd948b4ab4e0cc558ad2e8d795a6111f10
nam-gotit <lenkc98@gmail.com>
2019-01-17 11:08:00 +0700

    Move: move blog routes to blog controller

--- main/controllers/blog.py
+++ main/controllers/blog.py
@@ -1,15 +1,16 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from main import app
-from main.libs.dbsession import DBSession
+from main.libs.auth import authorization
 from main.libs.database import db
 from main.models.blog import Blog
+from main.models.like import Like
+from main.schemas.blog import BlogSchema
 
 
 # paging
 @app.route('/blogs')
 def get_all_blogs():
-    # session = DBSession()
     blogs = db.session.query(Blog).order_by(Blog.created_at.desc()).all()
     # use schema
     return jsonify(success=True, data=[b.serialize for b in blogs])
@@ -17,7 +18,6 @@ def get_all_blogs():
 
 @app.route('/blogs/<int:blog_id>', methods=['GET'])
 def get_blog_by_id(blog_id):
-    # session = DBSession()
     blog = db.session.query(Blog).filter_by(id=blog_id).first()
     return jsonify(success=True, data=blog.serialize)
 
@@ -25,9 +25,62 @@ def get_blog_by_id(blog_id):
 # use param
 @app.route('/blogs/trending')
 def get_trending_blogs():
-    # session = DBSession()
     # use CONSTANT for limit
     blogs = db.session.query(Blog).order_by(Blog.like.desc()).limit(3).all()
     return jsonify(success=True, data=[b.serialize for b in blogs])
 
 # use HTTP code instead of success = True
+
+# move to blog.py
+@app.route('/blogs', methods=['POST'])
+@authorization
+def post_blog(user_id):
+    schema = BlogSchema()
+    blog_valid = schema.load(request.get_json())
+    if len(blog_valid.errors) > 0:
+        #
+        return jsonify(blog_valid.errors)
+    new_blog = Blog(title=blog_valid.data["title"], body=blog_valid.data["body"], user_id=user_id)
+    db.session.add(new_blog)
+    db.session.commit()
+    return jsonify(success=True)
+
+
+@app.route('/blogs/<int:blog_id>', methods=['PUT'])
+@authorization
+def put_blog(user_id, blog_id):
+    schema = BlogSchema()
+    blog_valid = schema.load(request.get_json())
+    if len(blog_valid.errors) > 0:
+        return jsonify(blog_valid.errors)
+    edit_blog = db.session.query(Blog).filter_by(id=blog_id, user_id=user_id).first()
+    if edit_blog is None:
+        # Which error? action prohibit or blog not found??
+        return jsonify(error=True)
+    edit_blog.title = blog_valid.data['title']
+    edit_blog.body = blog_valid.data['body']
+    db.session.commit()
+    return jsonify(success=True)
+
+
+@app.route('/blogs/<int:blog_id>', methods=['DELETE'])
+@authorization
+def delete_blog(user_id, blog_id):
+    deleting_blog = db.session.query(Blog).filter_by(user_id=user_id, id=blog_id).first()
+    if deleting_blog is None:
+        return jsonify(error=True)
+    db.session.delete(deleting_blog)
+    db.session.commit()
+    return jsonify(success=True)
+
+
+# use likes, and POST instead of GET
+# write another decorator to check valid blog
+@app.route('/blogs/<int:blog_id>/like', methods=['GET'])
+@authorization
+def like_blog(user_id, blog_id):
+    # check if liked? before add like
+    like = Like(user_id=user_id, blog_id=blog_id)
+    db.session.add(like)
+    db.session.commit()
+    return jsonify(success=True)
--- main/controllers/user.py
+++ main/controllers/user.py
@@ -1,12 +1,9 @@
-from flask import request, jsonify
+from flask import jsonify
 
 from main import app
-from main.libs.auth import authorization
 from main.libs.database import db
 from main.models.blog import Blog
-from main.models.like import Like
 from main.models.user import User
-from main.schemas.blog import BlogSchema
 from main.schemas.user import UserSchema
 
 
@@ -23,60 +20,3 @@ def get_user_info(user_id):
 def get_blogs_by_user(user_id):
     blogs = db.session.query(Blog).filter_by(user_id=user_id).all()
     return jsonify(success=True, data=[b.serialize for b in blogs])
-
-
-# move to blog.py
-@app.route('/blogs', methods=['POST'])
-@authorization
-def post_blog(user_id):
-    schema = BlogSchema()
-    # req name??
-    req = schema.load(request.get_json())
-    if len(req.errors) > 0:
-        #
-        return jsonify(req.errors)
-    new_blog = Blog(title=req.data["title"], body=req.data["body"], user_id=user_id)
-    db.session.add(new_blog)
-    db.session.commit()
-    return jsonify(success=True)
-
-
-@app.route('/blogs/<int:blog_id>', methods=['PUT'])
-@authorization
-def put_blog(user_id, blog_id):
-    schema = BlogSchema()
-    req = schema.load(request.get_json())
-    if len(req.errors) > 0:
-        return jsonify(req.errors)
-    edit_blog = db.session.query(Blog).filter_by(id=blog_id, user_id=user_id).first()
-    if edit_blog is None:
-        # Which error? action prohibit or blog not found??
-        return jsonify(error=True)
-    # use '' for all, not ""
-    edit_blog.title = req.data["title"]
-    edit_blog.body = req.data["body"]
-    db.session.commit()
-    return jsonify(success=True)
-
-
-@app.route('/blogs/<int:blog_id>', methods=['DELETE'])
-@authorization
-def delete_blog(user_id, blog_id):
-    deleting_blog = db.session.query(Blog).filter_by(user_id=user_id, id=blog_id).first()
-    if deleting_blog is None:
-        return jsonify(error=True)
-    db.session.delete(deleting_blog)
-    db.session.commit()
-    return jsonify(success=True)
-
-
-# use likes, and POST instead of GET
-# write another decorator to check valid blog
-@app.route('/blogs/<int:blog_id>/like', methods=['GET'])
-@authorization
-def like_blog(user_id, blog_id):
-    # check if liked? before add like
-    like = Like(user_id=user_id, blog_id=blog_id)
-    db.session.add(like)
-    db.session.commit()
-    return jsonify(success=True)
