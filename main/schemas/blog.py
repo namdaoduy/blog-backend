e443ccd2bf208d19673d47f5ba74dff681be3c38
nam-gotit <lenkc98@gmail.com>
2019-01-22 09:36:38 +0700

    Refactor: use constant for length range of blog

--- main/schemas/blog.py
+++ main/schemas/blog.py
@@ -1,12 +1,15 @@
 from marshmallow import fields, validate
 
 from main.schemas.base import BaseSchema
+from main import config
 
 
 class BlogSchema(BaseSchema):
     id = fields.Integer()
-    title = fields.String(validate=validate.Length(min=10, max=100))
-    body = fields.String(validate=validate.Length(min=1000))
+    title = fields.String(validate=validate.Length(min=config.BLOG_TITLE_LENGTH_MIN,
+                                                   max=config.BLOG_TITLE_LENGTH_MAX))
+    body = fields.String(validate=validate.Length(min=config.BLOG_BODY_LENGTH_MIN,
+                                                  max=config.BLOG_BODY_LENGTH_MAX))
     like = fields.Integer()
     created_at = fields.DateTime()
     user_id = fields.String()
